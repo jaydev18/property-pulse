c7c3c9618829c60432fbf8da60cd27926849beaa
Jaydev <jaydev0141@gmail.com>
2024-04-03 10:47:24 +0530

    setup components: Hero, InfoBoxes and footer.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
   description: "Find your dream rental property",
@@ -14,6 +15,7 @@ const mainLayout = ({ children }) => {
         <main>
           <Navbar />
           {children}
+          <Footer />
         </main>
       </body>
     </html>
