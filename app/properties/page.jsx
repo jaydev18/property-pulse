0061e22d119d2c502d900ab72f3068a6c212ee0f
Jaydev <jaydev0141@gmail.com>
2024-04-03 14:37:35 +0530

    setup: property card

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 const PropertyPage = () => {
-  return <div>PropertyPage</div>;
+  return (
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default PropertyPage;
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
     <footer className="bg-gray-200 py-4 mt-24">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
         <div className="mb-4 md:mb-0">
-          <Image src={logo} alt="logo" class="h-8 w-auto" />
+          <Image src={logo} alt="logo" className="h-8 w-auto" />
         </div>
         <div>
           <p className="text-sm text-gray-500 mt-2 md:mt-0">
