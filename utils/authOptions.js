f2023247c885883ce7f11ef0fe0e9e835da9eae3
Jaydev <jaydev0141@gmail.com>
2024-04-11 14:30:56 +0530

    Next auth SignIn added. Bringing user image when signin

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,5 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
-
+import connectDB from "@/config/database";
+import User from "@/models/User";
 export const authOptions = {
   providers: [
     GoogleProvider({
@@ -18,14 +19,33 @@ export const authOptions = {
     //invocked on success signin
     async signIn({ profile }) {
       //1.conntect to the database
+      await connectDB();
       //2. check if user exists
+      const userExist = await User.findOne({
+        email: profile.email,
+      });
       //3. if not, then add user to database
+      if (!userExist) {
+        const username = profile.name.slice(0, 20);
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       //4. return true to allow sign in
+      return true;
     },
-    async session({ sessiion }) {
+    async session({ session }) {
       //1. get user from database
+      const user = await User.findOne({
+        email: session.user.email,
+      });
+      console.log(user);
       //2. Assign the usser id to the session
+      session.user.id = user._id.toString();
       //3. return session
+      return session;
     },
   },
 };
