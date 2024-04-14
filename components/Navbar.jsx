c33b5bc4346abef0b4d2ecaf01e9fa688cc187bf
Jaydev <jaydev0141@gmail.com>
2024-04-14 11:05:10 +0530

    Add Property Form & component State

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -22,8 +22,7 @@ const Navbar = () => {
     };
     setAuthProvider();
   }, []);
-  console.log(providers);
-  console.log(profileImage);
+
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
