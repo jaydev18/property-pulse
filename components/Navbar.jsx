4901e8120fcbead63207c0055227c582089d94e8
Jaydev <jaydev0141@gmail.com>
2024-04-14 10:15:21 +0530

    added: Signout fecture

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -190,6 +190,9 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-0"
+                      onClick={() => {
+                        setIsProfileOpen(false);
+                      }}
                     >
                       Your Profile
                     </Link>
@@ -199,10 +202,17 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
+                      onClick={() => {
+                        setIsProfileOpen(false);
+                      }}
                     >
                       Saved Properties
                     </Link>
                     <button
+                      onClick={() => {
+                        setIsProfileOpen(false);
+                        signOut();
+                      }}
                       className="block px-4 py-2 text-sm text-gray-700"
                       role="menuitem"
                       tabIndex="-1"
